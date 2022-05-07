3a927c876e19998825ecc9bc0eee735a5707b491
George Brignell-Cash <georgebrignellcash@gmail.com>
2022-05-07 17:54:06 +0100

    Many rays to one boundary

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
 import pygame
 from boundary import Boundary
 from ray import Ray
+import math
 
 #Initalise game
 pygame.init()
@@ -17,16 +18,17 @@ pygame.init()
 WHITE = (255,255,255)
 RED = (255,0,0)
 BLACK = (0,0,0)
-WIDTH = 600
-HEIGHT = 600
+WIDTH = 1000
+HEIGHT = 1000
 
 #Creating the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(WHITE)
 pygame.display.set_caption('2D RayTracer')
 
-#Boundary/s
+#Boundary/s and ray/s
 bound = Boundary(400,100,400,400)
+
 
 #Game loop
 running = True
@@ -37,16 +39,22 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-
     #Drawing screen and boundary
     screen.fill(BLACK)
     mouse_pos = pygame.mouse.get_pos()
     bound.draw(screen,WHITE)
     
 
+    rays = []
     #Create and draw ray
-    ray = Ray(10,250,mouse_pos[0],mouse_pos[1])
-    ray.cast(bound,screen,WHITE)
+    view_size = math.sqrt(WIDTH**2+HEIGHT**2)
+    for angle in range(0,360,5):
+        rad = angle * math.pi / 180
+        ray_ = Ray(mouse_pos[0],mouse_pos[1],mouse_pos[0] + view_size*math.cos(rad), mouse_pos[1] + view_size*math.sin(rad))
+        rays.append(ray_)
+
+    for ray in rays:
+        ray.cast(bound,screen,WHITE)
 
 
     #update screen
--- ray.py
+++ ray.py
@@ -20,12 +20,12 @@ class Ray():
             u = ((bound.x1 - self.x1) * (bound.y1 - bound.y2) - (bound.y1 - self.y1) * (bound.x1 - bound.x2))/den
             t = ((bound.x1 - self.x1) * (self.y1 - self.y2) - (bound.y1 - self.y1) * (self.x1 - self.x2))/den
 
-        if (t >= 0 and t<=1) and (u >= 0 and u <= 1):
-            #Point lines cross, drawn by circle and ray cut to that point.
-            point = (bound.x1 + t*(bound.x2 - bound.x1), bound.y1 + t*(bound.y2 - bound.y1))
-            pygame.draw.circle(screen, colour, point,4)
-            self.x2 = point[0]
-            self.y2 = point[1]
-            self.draw(screen,colour) 
-        else:
-            self.draw(screen,colour)
+            if (t >= 0 and t<=1) and (u >= 0 and u <= 1):
+                #Point lines cross, drawn by circle and ray cut to that point.
+                point = (bound.x1 + t*(bound.x2 - bound.x1), bound.y1 + t*(bound.y2 - bound.y1))
+                pygame.draw.circle(screen, colour, point,4)
+                self.x2 = point[0]
+                self.y2 = point[1]
+                self.draw(screen,colour) 
+            # else:
+            #     self.draw(screen,colour)
